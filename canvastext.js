76276370a1cd8b22dac63b6c26bac0fb7c227d96
masa <79982360+masatojo@users.noreply.github.com>
2022-02-25 12:44:22 +0900

    Create canvastext.js

---
+++ canvastext.js
@@ -0,0 +1,33 @@
+function loadImage(id)
+{
+	//画像を読み込んでImageオブジェクトを作成する
+	var image = new Image();
+	image.src = 'canvas.jpg';
+	image.onload = (function () {
+		//画像ロードが完了してからキャンバスの準備をする
+		var canvas = document.getElementById(id);
+		var ctx = canvas.getContext('2d');
+		//キャンバスのサイズを画像サイズに合わせる
+		canvas.width = image.width;
+		canvas.height = image.height;
+		//キャンバスに画像を描画（開始位置0,0）
+		ctx.drawImage(image, 0, 0);
+	});
+}
+//キャンバスに文字を描く
+function drawText(canvas_id, text_id)
+{
+	var canvas = document.getElementById(canvas_id);
+	var ctx = canvas.getContext('2d');
+	var text = document.getElementById(text_id);
+	//文字のスタイルを指定
+	ctx.font = '32px serif';
+	ctx.fillStyle = '#404040';
+	//文字の配置を指定（左上基準にしたければtop/leftだが、文字の中心座標を指定するのでcenter
+	ctx.textBaseline = 'center';
+	ctx.textAlign = 'center';
+	//座標を指定して文字を描く（座標は画像の中心に）
+	var x = (canvas.width / 2);
+	var y = (canvas.height / 2);
+	ctx.fillText(text.value, x, y);
+}
